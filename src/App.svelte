bc319c7979030c4ee0dfcd07d96463dc5bced198
YanMaker <94195591+YanMaker@users.noreply.github.com>
2022-11-08 14:18:57 -0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -50,6 +50,9 @@
       previousoperation = "";
       currentnumber = Number(previousnumber) / Number(currentnumber);
     }
+    if (currentnumber == "Infinity") {
+    currentnumber = "Calculation outside of expected range";
+    }
   }
 </script>
 
